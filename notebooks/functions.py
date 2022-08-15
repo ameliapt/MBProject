229d1599eeb8c80d9e3208e8c340523f15a7c324
Amelia Pijuan <ameliapijuan@gmail.com>
2022-08-15 23:37:57 +0200

    Functions updated

--- notebooks/functions.py
+++ notebooks/functions.py
@@ -1,6 +1,11 @@
-
 import datetime
 import pandas as pd
+
+def get_data_and_create_excel_file(url, file_name, path):
+    df = pd.read_json(url)
+    df.to_excel(path+file_name)
+    return df
+    
 
 def normalize_json(initial_df):
     
@@ -15,12 +20,12 @@ def normalize_json(initial_df):
     # Normalize json nested dictionary from original dataset
     for col in initial_df.index:
         data = pd.json_normalize(initial_df.Data[col])
-        data['Origin'] = initial_df.Nombre[col]
+        data['info'] = initial_df.Nombre[col]
         df = pd.concat([df, data])
      
     # Change column order
-    col = df.pop('Origin')
-    df.insert(loc= 0 , column= 'Origin', value= col)
+    col = df.pop('info')
+    df.insert(loc= 0 , column= 'info', value= col)
     
     # Drop innecesary columns
     df = df.drop(['Anyo', 'FK_TipoDato', 'FK_Periodo', 'Secreto'], axis=1)
